34d5f94df86df06f312813cd3cf72460fa4d02e4
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-07-08 19:57:09 -0300

    First Steps

---
+++ back-end/escopo.js
@@ -0,0 +1,11 @@
+let estudante;
+
+estudante = 'Enzo'
+console.log(estudante)
+
+
+if (true){
+    estudante = 'Caroline';
+    console.log(estudante);
+}
+console.log(estudante)
---
+++ back-end/variaveis.js
@@ -0,0 +1,6 @@
+let estudante = 'Enzo';
+let professora = 'Mari';
+var estudante_var = 'Enzooo'
+const estudante_nao_alteravel = '5NZ0'
+
+console.log(estudante1, professora);
