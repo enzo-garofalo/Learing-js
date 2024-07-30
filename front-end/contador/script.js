f4eadd0d0bd15a25c6e2de86f4b282eb2a39900a
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-07-30 10:07:50 -0300

    Contador com estilo

--- front-end/contador/script.js
+++ front-end/contador/script.js
@@ -1,21 +1,39 @@
 const dec = document.querySelector('#boton2');
 const inc = document.querySelector('#boton1');
-const num = document.querySelector('#numero');
+const num = document.querySelector('.numero');
 
 let number = 0
 
 dec.addEventListener('click', () => {
     number--;
     mostrarNum();
+    num.classList.add('animacion2');
+    setTimeout(() => num.classList.remove('animacion2'), 100);
 });
 
 inc.addEventListener('click', () => {
     number++;
     mostrarNum();
+    num.classList.add('animacion');
+    setTimeout(() => num.classList.remove('animacion'), 100);
 });
 
 function mostrarNum(){
+    corDoTexto(number);
     num.innerHTML = `${number}`;
+}
+
+function corDoTexto(number){
+    if (number > 0){
+        num.classList.remove('negativo')
+        num.classList.add('positivo')
+    } else if (number < 0){
+        num.classList.remove('positivo')
+        num.classList.add('negativo')
+    } else {
+        num.classList.remove('positivo')
+        num.classList.remove('negativo')
+    }
 }
 
 mostrarNum();
